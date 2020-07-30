dd42c42d4dc81f3dfe0862d2e35cda08e1a5f8fc
MscrmTools <tanguy92@hotmail.com>
2020-07-30 12:14:09 +0200

    Added new control ActionButton

---
+++ ActionButton/ActionButton/ButtonControl.tsx
@@ -0,0 +1,35 @@
+import * as React from 'react'
+import {Stack} from 'office-ui-fabric-react/lib/components/Stack';
+import {PrimaryButton} from 'office-ui-fabric-react/lib/components/Button';
+
+export interface IButtonProps {
+    text: string,
+    onClick: () => void
+    disabled: boolean,
+  }
+
+export interface IButtonState {
+  text: string,
+  onClick: () => void
+  disabled: boolean,
+
+}
+
+export default class ButtonControl extends React.Component<IButtonProps, IButtonState>{
+  constructor(props :IButtonProps){
+    super(props);
+    this.state = {
+      text:props.text,
+      onClick: props.onClick,
+      disabled: false
+    }
+  }
+    render(){
+        return(
+          <Stack horizontal>
+            <PrimaryButton text={this.state.text} disabled={this.state.disabled} onClick={this.state.onClick}/>
+          </Stack>
+                
+        );
+    }
+}
---
+++ ActionButton/ActionButton/index.ts
@@ -0,0 +1,95 @@
+import {IInputs, IOutputs} from "./generated/ManifestTypes";
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import ButtonControl, { IButtonProps } from "./ButtonControl";
+
+export class ActionButton implements ComponentFramework.StandardControl<IInputs, IOutputs> {
+
+	private container: HTMLDivElement;
+	private notifyOutputChanged: () => void;
+	private currentValue:string|null;
+	private actionText:string;
+	/**
+	 * Empty constructor.
+	 */
+	constructor()
+	{
+		
+	}
+
+	/**
+	 * Used to initialize the control instance. Controls can kick off remote server calls and other initialization actions here.
+	 * Data-set values are not initialized here, use updateView.
+	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to property names defined in the manifest, as well as utility functions.
+	 * @param notifyOutputChanged A callback method to alert the framework that the control has new outputs ready to be retrieved asynchronously.
+	 * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
+	 * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
+	 */
+	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement)
+	{
+		this.container = container;
+		this.notifyOutputChanged = notifyOutputChanged;
+
+		this.actionText = context.parameters.ActionText.raw??"";
+
+		if(this.actionText.trim().startsWith("{")){
+			let json = JSON.parse(this.actionText);
+			try{
+			this.actionText = json[context.userSettings.languageId];
+			if(this.actionText === undefined){
+				this.actionText = json[parseInt(Object.keys(json)[0])];
+			}
+			}
+			catch{
+				this.actionText = json[parseInt(Object.keys(json)[0])];
+			}
+		}
+
+		this.renderControl(context);
+	}
+
+	private renderControl(context: ComponentFramework.Context<IInputs>): void {
+
+		let props: IButtonProps = {
+			text: this.actionText,
+			disabled: false, //context.parameters.SetAsFormState.raw === "True" ? context.mode.isControlDisabled : false,
+			onClick: () => {
+				this.notifyOutputChanged();
+			}
+		};
+
+		ReactDOM.render(React.createElement(ButtonControl, props), this.container);
+	}
+
+
+	/**
+	 * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
+	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
+	 */
+	public updateView(context: ComponentFramework.Context<IInputs>): void
+	{
+		this.renderControl(context);
+
+		this.currentValue = context.parameters.BoundAttribute.raw?? "";
+	}
+
+	/** 
+	 * It is called by the framework prior to a control receiving new data. 
+	 * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
+	 */
+	public getOutputs(): IOutputs
+	{
+		return {
+			BoundAttribute: this.actionText
+		};
+	}
+
+	/** 
+	 * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
+	 * i.e. cancelling any pending remote calls, removing listeners, etc.
+	 */
+	public destroy(): void
+	{
+		// Add code to cleanup control if necessary
+	}
+}
