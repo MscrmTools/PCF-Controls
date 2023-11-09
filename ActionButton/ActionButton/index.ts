f85caba1e4d2a732aefc9aaccd31b487ecf33bd1
Ahash Sritharan <me@ahash.dev>
2023-11-09 13:52:36 +0100

    Added Tooltip to ActionButton & German translation

--- ActionButton/ActionButton/index.ts
+++ ActionButton/ActionButton/index.ts
@@ -1,25 +1,23 @@
-import {IInputs, IOutputs} from "./generated/ManifestTypes";
+import { IInputs, IOutputs } from "./generated/ManifestTypes";
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import ButtonControl, { IButtonControlProps } from "./ButtonControl";
 import { initializeIcons } from '@fluentui/font-icons-mdl2';
-import { AwayStatusIcon, SettingsIcon } from '@fluentui/react-icons';
 
 
 export class ActionButton implements ComponentFramework.StandardControl<IInputs, IOutputs> {
 
 	private container: HTMLDivElement;
 	private notifyOutputChanged: () => void;
-	private currentValue:string|null;
-	private actionText:string;
-	private id:string;
-	private sendId:boolean;
-	private  controlType : string;
+	private currentValue: string | null;
+	private actionText: string;
+	private id: string;
+	private sendId: boolean;
+	private controlType: string;
 	/**
 	 * Empty constructor.
 	 */
-	constructor()
-	{
+	constructor() {
 		initializeIcons();
 	}
 
@@ -31,27 +29,26 @@ export class ActionButton implements ComponentFramework.StandardControl<IInputs,
 	 * @param state A piece of data that persists in one session for a single user. Can be set at any point in a controls life cycle by calling 'setControlState' in the Mode interface.
 	 * @param container If a control is marked control-type='standard', it will receive an empty div element within which it can render its content.
 	 */
-	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container:HTMLDivElement)
-	{
+	public init(context: ComponentFramework.Context<IInputs>, notifyOutputChanged: () => void, state: ComponentFramework.Dictionary, container: HTMLDivElement) {
 		this.container = container;
 		this.notifyOutputChanged = notifyOutputChanged;
 
-		this.actionText = context.parameters.ActionText.raw??"";
-		this.id = context.parameters.Id.raw??"";
-		this.sendId = context.parameters.SendId.raw  === "1";
+		this.actionText = context.parameters.ActionText.raw ?? "";
+		this.id = context.parameters.Id.raw ?? "";
+		this.sendId = context.parameters.SendId.raw === "1";
 
 		// @ts-ignore
 		this.controlType = context.parameters.BoundAttribute.attributes.Type;
 
-		if(this.actionText.trim().startsWith("{")){
+		if (this.actionText.trim().startsWith("{")) {
 			let json = JSON.parse(this.actionText);
-			try{
-			this.actionText = json[context.userSettings.languageId];
-			if(this.actionText === undefined){
-				this.actionText = json[parseInt(Object.keys(json)[0])];
+			try {
+				this.actionText = json[context.userSettings.languageId];
+				if (this.actionText === undefined) {
+					this.actionText = json[parseInt(Object.keys(json)[0])];
+				}
 			}
-			}
-			catch{
+			catch {
 				this.actionText = json[parseInt(Object.keys(json)[0])];
 			}
 		}
@@ -61,7 +58,7 @@ export class ActionButton implements ComponentFramework.StandardControl<IInputs,
 
 	private renderControl(context: ComponentFramework.Context<IInputs>): void {
 
-		let props :IButtonControlProps={
+		let props: IButtonControlProps = {
 			text: this.actionText,
 			disabled: context.parameters.EnableButtonOnDisabledForm.raw === "1" ? false : context.mode.isControlDisabled,
 			style: {
@@ -77,7 +74,8 @@ export class ActionButton implements ComponentFramework.StandardControl<IInputs,
 			checkedBorderColor: context.parameters.PressedBorderColor.raw ?? "#0078d4",
 			checkedColor: context.parameters.PressedColor.raw ?? "#FFFFFF",
 			iconName: context.parameters.IconName.raw,
-			onClick: () => { this.notifyOutputChanged(); }			
+			toolTip: context.parameters.ToolTip.raw ?? "",
+			onClick: () => { this.notifyOutputChanged(); }
 		}
 
 		ReactDOM.render(React.createElement(ButtonControl, props), this.container);
@@ -88,23 +86,21 @@ export class ActionButton implements ComponentFramework.StandardControl<IInputs,
 	 * Called when any value in the property bag has changed. This includes field values, data-sets, global values such as container height and width, offline status, control metadata values such as label, visible, etc.
 	 * @param context The entire property bag available to control via Context Object; It contains values as set up by the customizer mapped to names defined in the manifest, as well as utility functions
 	 */
-	public updateView(context: ComponentFramework.Context<IInputs>): void
-	{
+	public updateView(context: ComponentFramework.Context<IInputs>): void {
 		this.renderControl(context);
 
-		this.currentValue = context.parameters.BoundAttribute.raw?? "";
+		this.currentValue = context.parameters.BoundAttribute.raw ?? "";
 	}
 
 	/** 
 	 * It is called by the framework prior to a control receiving new data. 
 	 * @returns an object based on nomenclature defined in manifest, expecting object[s] for property marked as “bound” or “output”
 	 */
-	public getOutputs(): IOutputs
-	{
-		if(this.controlType === "string"){
+	public getOutputs(): IOutputs {
+		if (this.controlType === "string") {
 			return {
 				BoundAttribute: this.sendId ? this.id : this.actionText
-			};	
+			};
 		}
 
 		return {
@@ -116,8 +112,7 @@ export class ActionButton implements ComponentFramework.StandardControl<IInputs,
 	 * Called when the control is to be removed from the DOM tree. Controls should use this call for cleanup.
 	 * i.e. cancelling any pending remote calls, removing listeners, etc.
 	 */
-	public destroy(): void
-	{
+	public destroy(): void {
 		// Add code to cleanup control if necessary
 	}
 }
